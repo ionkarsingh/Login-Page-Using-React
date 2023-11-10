35ce2f7f8cadd27ba27d8da6e36ef97665be746c
ionkarsingh <onkaryou7@gmail.com>
2023-11-10 18:28:08 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+// App.js
+
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Add your login logic here
+    console.log('Login clicked');
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Login</h2>
+        <form>
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Enter your username"
+            />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your password"
+            />
+          </div>
+
+          <button type="button" onClick={handleLogin}>
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default App;
